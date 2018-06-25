d0f1a426cbe9fd789efd562034a58cc115092919
Jack <bear30921@athena.com.tw>
2018-06-25 10:42:00 +0800

    webpack配置增加babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,16 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, './dist'),
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
 };
